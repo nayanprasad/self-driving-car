845f8dd6d807997249f57ca6cd4d7c2a36059e97
Nayan Prasad P K <nayanprasad096@gmail.com>
2023-03-28 03:04:24 +0530

    added car in the canvas

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d")
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
