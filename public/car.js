b6328121aaaedca8de7bc4ca508729e62c245e56
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-10-23 18:22:15 +0530

    feat: stop at boundary

--- public/car.js
+++ public/car.js
@@ -1,63 +1,74 @@
 class Car {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+    constructor(x, y, width, height, boundaries) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
 
-    this.img = new Image();
-    this.img.src = "./assets/car.png";
+        this.boundaries = boundaries;
 
-    this.speed = 0;
-    this.acceleration = 0.2;
-    this.maxSpeed = 3;
-    this.friction = 0.05;
-    this.angle = 0;
+        this.img = new Image();
+        this.img.src = "./assets/car.png";
 
-    this.controls = new Controls();
-  }
-
-  update() {
-    if(this.controls.forward)
-        this.speed += this.acceleration;
-    if(this.controls.reverse)
-        this.speed -= this.acceleration;
-
-    if(this.speed > this.maxSpeed)
-        this.speed = this.maxSpeed;
-    if(this.speed < -this.maxSpeed/2)
-        this.speed = -this.maxSpeed/2;
-
-    if(this.speed > 0)
-        this.speed -= this.friction;
-    if(this.speed < 0)
-        this.speed += this.friction;
-    if(Math.abs(this.speed) < this.friction)
         this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
 
-    if(this.speed !== 0) {
-        const flip = this.speed > 0 ? 1 : -1;
-        if(this.controls.left)
-            this.angle += 0.03 * flip;
-        if(this.controls.right)
-            this.angle -= 0.03 * flip;
+        this.controls = new Controls();
     }
 
-    this.x -= Math.sin(this.angle) * this.speed;
-    this.y -= Math.cos(this.angle) * this.speed;
-  }
+    update() {
+        if (this.controls.forward)
+            this.speed += this.acceleration;
+        if (this.controls.reverse)
+            this.speed -= this.acceleration;
+
+        if (this.speed > this.maxSpeed)
+            this.speed = this.maxSpeed;
+        if (this.speed < -this.maxSpeed / 2)
+            this.speed = -this.maxSpeed / 2;
+
+        if (this.speed > 0)
+            this.speed -= this.friction;
+        if (this.speed < 0)
+            this.speed += this.friction;
+        if (Math.abs(this.speed) < this.friction)
+            this.speed = 0;
+
+        if (this.speed !== 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left)
+                this.angle += 0.03 * flip;
+            if (this.controls.right)
+                this.angle -= 0.03 * flip;
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+
+        this.checkBoundaries();
+    }
+
+    checkBoundaries() {
+        if (this.x > this.boundaries[0][0].x - this.width)
+            this.x = this.boundaries[0][0].x - this.width
+        if (this.x < this.boundaries[1][0].x + this.width)
+            this.x = this.boundaries[1][0].x + this.width;
+    }
 
 
-  draw(ctx) {
+    draw(ctx) {
 
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
 
-    ctx.beginPath();
-    ctx.drawImage(this.img, -this.width/2, -this.height/2, this.width, this.height);
+        ctx.beginPath();
+        ctx.drawImage(this.img, -this.width / 2, -this.height / 2, this.width, this.height);
 
-    ctx.restore();
+        ctx.restore();
 
-  }
+    }
 }
--- public/road.js
+++ public/road.js
@@ -1,41 +1,50 @@
 class Road {
-    constructor(x, width, lanes = 4) {
+    constructor(x, width, lanes = 3) {
         this.x = x;
-        this.width = width ;
+        this.width = width;
         this.lanes = lanes;
 
-        this.diff = width/lanes;
-        this.left = x - width/2 + 20;
-        this.right = x + width/2 - 20;
+        this.diff = width / lanes;
+        this.left = x - width / 2 + 20;
+        this.right = x + width / 2 - 20;
 
         const infinity = 10e6;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = {x: this.left, y: this.top};
+        const topRight = {x: this.right, y: this.top};
+        const bottomLeft = {x: this.left, y: this.bottom};
+        const bottomRight = {x: this.right, y: this.bottom};
+
+        this.boundaries = [[topRight, bottomRight], [topLeft, bottomLeft]];
     }
 
-    getRoadCenter(laneIndex = this.lanes/2) {
-        const index = laneIndex < 0 || laneIndex > this.lanes ? this.lanes/2  : laneIndex;
-        return this.lanes % 2 === 0 ? this.diff * index + this.diff/2 : this.diff * index;
+    getRoadCenter(laneIndex = this.lanes / 2) {
+        const index = laneIndex < 0 || laneIndex > this.lanes ? this.lanes / 2 : laneIndex;
+        return this.lanes % 2 === 0 ? this.diff * index + this.diff / 2 : this.diff * index;
     }
 
     draw(ctx) {
         ctx.lineWidth = 8;
         ctx.strokeStyle = "white";
 
-        for(let i = 0; i <= this.lanes; i++) {
-            if(i === 0 || i === this.lanes) {
-                ctx.strokeStyle = "white";
-                ctx.setLineDash([])
-                ctx.lineWidth = 20;
-            }
-            else {
-                ctx.setLineDash([50, 30])
-                ctx.lineWidth = 8;
-            }
+        ctx.setLineDash([50, 30])
+        ctx.lineWidth = 8;
+        for (let i = 1; i < this.lanes; i++) {
             ctx.beginPath();
             ctx.moveTo(this.diff * i, this.top);
             ctx.lineTo(this.diff * i, this.bottom);
             ctx.stroke();
         }
+
+        ctx.setLineDash([])
+        ctx.lineWidth = 15;
+        ctx.beginPath();
+        ctx.moveTo(this.boundaries[0][0].x, this.boundaries[0][0].y);
+        ctx.lineTo(this.boundaries[0][1].x, this.boundaries[0][1].y);
+        ctx.moveTo(this.boundaries[1][0].x, this.boundaries[1][0].y);
+        ctx.lineTo(this.boundaries[1][1].x, this.boundaries[1][1].y);
+        ctx.stroke();
     }
 }
