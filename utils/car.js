5b26336c577a8e6837aab00ceef148b21e2ca84f
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-17 23:08:07 +0530

    detect collision on roads

--- utils/car.js
+++ utils/car.js
@@ -1,73 +1,115 @@
-class Car {
-    constructor(x, y, width, height, boundaries) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.boundaries = boundaries;
+class Car{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
+        this.damaged=false;
 
         this.img = new Image();
         this.img.src = "./assets/car.png";
 
-        this.speed = 0;
-        this.acceleration = 0.2;
-        this.maxSpeed = 3;
-        this.friction = 0.05;
-        this.angle = 0;
-
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        this.sensor=new Sensor(this);
+        this.controls=new Controls();
     }
 
-    update(boundaries) {
-        if (this.controls.forward)
-            this.speed += this.acceleration;
-        if (this.controls.reverse)
-            this.speed -= this.acceleration;
+    update(roadBorders){
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders);
+        }
+        this.sensor.update(roadBorders);
+    }
 
-        if (this.speed > this.maxSpeed)
-            this.speed = this.maxSpeed;
-        if (this.speed < -this.maxSpeed / 2)
-            this.speed = -this.maxSpeed / 2;
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
 
-        if (this.speed > 0)
-            this.speed -= this.friction;
-        if (this.speed < 0)
-            this.speed += this.friction;
-        if (Math.abs(this.speed) < this.friction)
-            this.speed = 0;
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
+    }
 
-        if (this.speed !== 0) {
-            const flip = this.speed > 0 ? 1 : -1;
-            if (this.controls.left)
-                this.angle += 0.03 * flip;
-            if (this.controls.right)
-                this.angle -= 0.03 * flip;
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
         }
 
-        this.x -= Math.sin(this.angle) * this.speed;
-        this.y -= Math.cos(this.angle) * this.speed;
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
 
-        this.checkBoundaries(boundaries);
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
 
-        this.sensor.update(boundaries);
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
 
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
-
-    checkBoundaries(boundaries) {
-        if (this.x > boundaries[0][0].x - this.width)
-            this.x = boundaries[0][0].x - this.width
-        if (this.x < boundaries[1][0].x + this.width)
-            this.x = boundaries[1][0].x + this.width;
-    }
-
 
     draw(ctx) {
+        if(this.damaged){
+            ctx.fillStyle = "gray";
+        } else {
+            ctx.fillStyle = "black";
+        }
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
         ctx.beginPath();
-            ctx.drawImage(this.img, -this.width / 2, -this.height / 2, this.width, this.height);
+        ctx.drawImage(this.img, -this.width / 2, -this.height / 2, this.width, this.height);
         ctx.restore();
 
         this.sensor.draw(ctx);
--- utils/util.js
+++ utils/util.js
@@ -27,3 +27,20 @@ function getDistance(x1, y1, x2, y2) {
     return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 }
 
+
+function polysIntersect(poly1, poly2){
+    for(let i=0;i<poly1.length;i++){
+        for(let j=0;j<poly2.length;j++){
+            const touch=getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if(touch){
+                return true;
+            }
+        }
+    }
+    return false;
+}
