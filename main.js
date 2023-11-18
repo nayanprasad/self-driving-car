a12d5a7b9e3bc4d0b91e59f51f4fe668b46a09f6
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-18 16:14:18 +0530

    fix traffic cars collision

--- main.js
+++ main.js
@@ -5,19 +5,31 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width);
 const car = new Car(road.getRoadCenter(), 900, 40, 60, road.boundaries);
 const traffic = [];
-for (let i = 0; i < 20; i++) {
-    traffic.push(new Traffic(
-        road.getRoadCenter() - 150 + Math.random() * 300,
-        700 - i * 150,
-        40,
-        60,
-        Math.random() * 3 + 0.5
-    ));
-}
+
 
 animate();
 
 function animate() {
+    for (let i = 0; i < 10000; i++) {
+        const trafficCar = {
+            x: road.getRoadCenter() - 150 + Math.random() * 300,
+            y: 700 - i * 150,
+            width: 40,
+            height: 60,
+            speed: Math.random() * 3.5 + 0.5
+        }
+
+        let isCollision = false;
+        traffic.forEach((e) => {
+            if(e.x > trafficCar.x - trafficCar.width && e.x < trafficCar.x + trafficCar.width){
+                isCollision = true;
+            }
+        });
+
+        if(!isCollision){
+            traffic.push(new Traffic(trafficCar.x, trafficCar.y, trafficCar.width, trafficCar.height, trafficCar.speed));
+        }
+    }
     car.update(road.boundaries, traffic);
     traffic.forEach((e) => e.update());
     canvas.height = window.innerHeight;
